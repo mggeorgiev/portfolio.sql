93c0f39e307dc791850f9a8c4b46901a3c6812a9
Martin Georgiev <mggeorgiev@gmail.com>
2023-01-23 21:27:15 +0100

    create views

--- cantine/action.sql
+++ cantine/action.sql
@@ -25,21 +25,86 @@ SELECT
 FROM [portfolio].[cantine].[items]
 JOIN [portfolio].[cantine].[consumption] on [portfolio].[cantine].[items].[Id] = [portfolio].[cantine].[consumption].[itemId]
 GROUP BY ROLLUP ([name]);
+GO
 
-WITH RecursivePrices (ctn, name_one, name_two, price_one, price_two, combo_price) AS
+DECLARE @MyCounter INT;
+SET @MyCounter = 100;
+
+WITH RecursivePrices (ctn, name_one, name_two, price_one, price_two, combo_price, combo_price_multiplied) AS
 (
     SELECT
-        10 as ctn
+        @MyCounter as ctn
         ,t2.name as name_one
         ,t1.name as name_two
         ,t1.unitPrice as price_one
         ,t2.unitPrice as price_two
-        ,(t1.unitPrice + t2.unitPrice)*1 as combo_price
+        ,(t1.unitPrice + t2.unitPrice) as combo_price
+        ,(t1.unitPrice + t2.unitPrice)*@MyCounter as combo_price_multiplied
     FROM
         [portfolio].[cantine].[items] t1
     CROSS JOIN [portfolio].[cantine].[items] t2 
 
 )
-SELECT ctn, name_one, name_two, price_one, price_two, combo_price FROM RecursivePrices
---WHERE (combo_price % 10 = 0) ;
+SELECT ctn, name_one, name_two, price_one, price_two, combo_price, combo_price_multiplied FROM RecursivePrices
+WHERE (combo_price_multiplied % 0.2 = 0) 
+  AND price_one <> '0.00'
+  AND price_two <> '0.00';
+GO
+
+CREATE OR ALTER VIEW [cantine].V_CONSUMPTION_RESTARTED AS
+SELECT TOP (1000) [Id]
+      ,[ConsumptionDate]
+      ,[ConsumptionTime]
+      , DATEPART(dw, [ConsumptionDate]) as weekday
+      , DATEPART(hh, [ConsumptionTime]) as hours
+      , DATEPART(mi, [ConsumptionTime]) as minutes
+      ,[Product]
+      ,[Price]
+      ,SUM ([Price]) OVER (ORDER BY [Id]) AS RunningTotal
+  FROM (
+    SELECT TOP (1000) [Id]
+        ,[ConsumptionDate]
+        ,[ConsumptionTime]
+        , DATEPART(dw, [ConsumptionDate]) as weekday
+        , DATEPART(hh, [ConsumptionTime]) as hours
+        , DATEPART(mi, [ConsumptionTime]) as minutes
+        ,[Product]
+        ,[Price]
+    FROM [portfolio].[cantine].[consumption_log]
+    WHERE [ConsumptionDate] > CONVERT(date, '16-01-2023', 103) 
+  ) filtered;
+  GO
+
+SELECT TOP (1000) [Id]
+      ,[ConsumptionDate]
+      ,[ConsumptionTime]
+      , DATEPART(dw, [ConsumptionDate]) as weekday
+      , DATEPART(hh, [ConsumptionTime]) as hours
+      , DATEPART(mi, [ConsumptionTime]) as minutes
+      ,[Product]
+      ,[Price]
+  FROM [portfolio].[cantine].[consumption_log]
+  WHERE [ConsumptionDate] > CONVERT(date, '16-01-2023', 103);
+GO
+
+CREATE OR ALTER VIEW [cantine].V_CONSUMPTION AS
+SELECT TOP (1000) [Id]
+      ,[ConsumptionDate]
+      ,[ConsumptionTime]
+      , DATEPART(dw, [ConsumptionDate]) as weekday
+      , DATEPART(hh, [ConsumptionTime]) as hours
+      , DATEPART(mi, [ConsumptionTime]) as minutes
+      ,[Product]
+      ,[Price]
+  FROM [portfolio].[cantine].[consumption_log];
+GO
+
+SELECT * FROM [portfolio].[cantine].V_CONSUMPTION
+WHERE [ConsumptionDate] > CONVERT(date, '16-01-2023', 103)
+ORDER BY [ConsumptionDate] ASC, [ConsumptionTime];
+GO
+
+SELECT * FROM [portfolio].[cantine].V_CONSUMPTION_RESTARTED
+WHERE [ConsumptionDate] > CONVERT(date, '16-01-2023', 103)
+ORDER BY [ConsumptionDate] ASC, [ConsumptionTime];
 GO
