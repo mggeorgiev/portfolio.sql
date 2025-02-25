6b7760318c070c9054f4bfc072a63fddbcceb888
Martin Georgiev <mggeorgiev@gmail.com>
2025-02-25 07:08:13 +0100

    add: dependancies

--- township/actions.sql
+++ township/actions.sql
@@ -45,3 +45,40 @@ SELECT TOP (1000)
 GO
 
 SELECT * FROM [township].[V_CONSTRAINTS];
+GO
+
+CREATE OR ALTER VIEW [township].[V_CONSTRAINTS_DEPENDANCIES] AS
+SELECT TOP (1000) 
+    [portfolio].[township].[items].[Id],
+    [portfolio].[township].[items].[name] as [itemName],
+    [portfolio].[township].[constraints].[name] AS [constraintName],
+    [portfolio].[township].[items].[productiontime] as [productionTime],
+    parentDetails.name AS [parentName],
+    [portfolio].[township].[dependancies].[items] AS [numberOfItems]
+  FROM [portfolio].[township].[items]
+  JOIN [portfolio].[township].[constraints] ON [portfolio].[township].[items].[constraintId] = [portfolio].[township].[constraints].[Id]
+  LEFT JOIN [portfolio].[township].[dependancies] ON [portfolio].[township].[items].[Id] = [portfolio].[township].[dependancies].[itemId]
+  JOIN (SELECT * FROM [portfolio].[township].[items]) as parentDetails ON [portfolio].[township].[dependancies].[parentId] = parentDetails.Id
+  WHERE [portfolio].[township].[dependancies].[parentId] IS NULL
+
+UNION ALL
+
+SELECT TOP (1000) 
+    [portfolio].[township].[items].[Id],
+    [portfolio].[township].[items].[name] as [itemName],
+    [portfolio].[township].[constraints].[name] AS [constraintName],
+    [portfolio].[township].[items].[productiontime] as [productionTime],
+    parentDetails.name AS [parentName],
+    [portfolio].[township].[dependancies].[items] AS [numberOfItems]
+  FROM [portfolio].[township].[items]
+  JOIN [portfolio].[township].[constraints] ON [portfolio].[township].[items].[constraintId] = [portfolio].[township].[constraints].[Id]
+  LEFT JOIN [portfolio].[township].[dependancies] ON [portfolio].[township].[items].[Id] = [portfolio].[township].[dependancies].[itemId]
+  JOIN (SELECT * FROM [portfolio].[township].[items]) as parentDetails ON [portfolio].[township].[dependancies].[parentId] = parentDetails.Id
+  WHERE [portfolio].[township].[dependancies].[parentId] IS NOT NULL
+  ORDER BY productionTime, itemName;
+GO
+
+SELECT * FROM [portfolio].[township].[V_CONSTRAINTS_DEPENDANCIES];
+GO
+
+
