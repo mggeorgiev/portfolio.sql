c86641f9d54b1ba9c50a64639c29243a782bc5fc
Martin Georgiev <mggeorgiev@gmail.com>
2025-03-01 07:57:42 +0100

    add: PROCEDURE township.ProductionLine

--- township/actions.sql
+++ township/actions.sql
@@ -81,4 +81,127 @@ GO
 SELECT * FROM [portfolio].[township].[V_CONSTRAINTS_DEPENDANCIES];
 GO
 
+CREATE OR ALTER VIEW [township].[V_PRODUCTION_FLOW_PIZZA] AS
+WITH RecursiveCTE AS (
+    SELECT 
+        d.itemId
+        ,d.parentId
+        ,p.name AS parentName
+        ,i.name AS itemName
+        ,i.productiontime AS productionTime
+        ,c.name AS constraintName
+        ,d.items
+        , 1 as [level]
+    FROM 
+        portfolio.township.dependancies d
+    JOIN 
+        portfolio.township.items i ON i.Id = d.itemId
+    JOIN 
+        portfolio.township.items p ON p.Id = d.parentId
+    JOIN 
+        portfolio.township.constraints c ON c.Id = i.constraintId
+    WHERE 
+        i.name = 'pizza'
 
+    UNION ALL
+
+    SELECT 
+        d.itemId
+        ,d.parentId
+        ,p.name AS parentName
+        ,i.name AS itemName
+        ,i.productiontime AS productionTime
+        ,c.name AS constraintName
+        ,d.items
+        , rc.[level]+1 as [level]
+    FROM 
+        portfolio.township.dependancies d
+    JOIN 
+        portfolio.township.items i ON i.Id = d.itemId
+    JOIN 
+        portfolio.township.items p ON p.Id = d.parentId
+    JOIN 
+        portfolio.township.constraints c ON c.Id = i.constraintId
+    JOIN 
+        RecursiveCTE rc ON rc.parentId = d.itemId
+)
+SELECT 
+    * 
+FROM 
+    RecursiveCTE
+GO
+
+-- Create a new stored procedure called 'StoredProcedureName' in schema 'township'
+-- Drop the stored procedure if it already exists
+IF EXISTS (
+SELECT *
+  FROM INFORMATION_SCHEMA.ROUTINES
+WHERE SPECIFIC_SCHEMA = N'township'
+  AND SPECIFIC_NAME = N'ProductionLine'
+  AND ROUTINE_TYPE = N'PROCEDURE'
+)
+DROP PROCEDURE township.ProductionLine
+GO
+-- Create the stored procedure in the specified schema
+CREATE PROCEDURE township.ProductionLine
+  @itemName VARCHAR(50)
+-- add more stored procedure parameters here
+AS
+BEGIN
+    -- Print the constraintId (for demonstration purposes)
+    PRINT 'Processing item: ' + CAST(@itemName AS VARCHAR);
+    -- Run the RecursiveCTE query for the current @itemName
+    WITH RecursiveCTE AS (
+        SELECT 
+            d.itemId
+            ,d.parentId
+            ,p.name AS parentName
+            ,i.name AS itemName
+            ,i.productiontime AS productionTime
+            ,c.name AS constraintName
+            ,d.items AS requiredItems
+            , 1 as [level]
+        FROM 
+            portfolio.township.dependancies d
+        JOIN 
+            portfolio.township.items i ON i.Id = d.itemId
+        JOIN 
+            portfolio.township.items p ON p.Id = d.parentId
+        JOIN 
+            portfolio.township.constraints c ON c.Id = i.constraintId
+        WHERE 
+            i.name = @itemName
+
+        UNION ALL
+
+        SELECT 
+            d.itemId
+            ,d.parentId
+            ,p.name AS parentName
+            ,i.name AS itemName
+            ,i.productiontime AS productionTime
+            ,c.name AS constraintName
+            ,d.items AS requiredItems
+            , rc.[level]+1 as [level]
+        FROM 
+            portfolio.township.dependancies d
+        JOIN 
+            portfolio.township.items i ON i.Id = d.itemId
+        JOIN 
+            portfolio.township.items p ON p.Id = d.parentId
+        JOIN 
+            portfolio.township.constraints c ON c.Id = i.constraintId
+        JOIN 
+            RecursiveCTE rc ON rc.parentId = d.itemId
+    )
+    SELECT 
+        * 
+    FROM 
+        RecursiveCTE
+    ORDER BY 
+        [level] desc, parentName, productionTime;
+END
+GO
+-- -- example to execute the stored procedure we just created
+EXECUTE township.ProductionLine @itemName='pizza'
+GO
